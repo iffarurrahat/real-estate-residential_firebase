132e31177c841b0aa285bce057eb921b0a41e529
iffarurrahat <iffarurrahat@gmail.com>
2024-06-06 21:18:56 +0600

    Login page design complated

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,9 +1,85 @@
+import loginImg from "./../../assets/login-bg.jpg";
+import { useState } from "react";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { Link } from "react-router-dom";
+
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div>
-      <p className="text-green-500">Login Page</p>
+    <div className="grid md:grid-cols-5 font-roboto">
+      <img
+        src={loginImg}
+        alt="Login Image"
+        className=" bg-red-300 md:col-span-3 h-screen w-full object-cover hidden md:block"
+      />
+      <div className="h-screen w-full flex items-center md:col-span-2 px-8 sm:px-10 md:px-0 md:pl-10">
+        <div>
+          <h2 className="text-2xl sm:text-3xl md:text-4xl mb-2 font-bold text-blue-600">
+            Welcome Back
+          </h2>
+          <p className="w-11/2 text-gray-800 mb-6 text-sm">
+            Welcome back to Smart Sight System. Log in to continue your
+            personalized experience and services.
+          </p>
+          <form>
+            <div>
+              <label className="mb-1 text-sm">Email</label> <br />
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                className="border px-4 py-2 rounded w-full md:w-2/3 placeholder:text-xs"
+              />
+            </div>
+            <div className="mt-2 relative md:inline-block min-w-0 md:w-2/3">
+              <label className="mb-1 text-sm">Password</label> <br />
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                placeholder="Password"
+                className="border px-4 py-2 rounded w-full placeholder:text-xs"
+              />
+              <span
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute inset-y-0 right-0 top-0 flex items-center pr-3 cursor-pointer"
+              >
+                {showPassword ? <AiFillEye /> : <AiFillEyeInvisible />}
+              </span>
+              <p className="text-xs mt-2 text-[#696969]">
+                <a href="#">Forgot password?</a>
+              </p>
+            </div>
+            <div className="mt-5 md:w-2/3">
+              <input
+                type="submit"
+                value="Login"
+                className="bg-blue-600 text-white text-sm rounded py-3 w-full cursor-pointer placeholder:text-2xl"
+              />
+            </div>
+          </form>
+          <p className="mt-10">
+            Need an account ?{" "}
+            <Link to="/register" className="text-blue-600 font-semibold">
+              Create Account
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Login;
+
+/*
+
+<h2 className="text-2xl sm:text-3xl md:text-4xl mb-2 font-bold">
+            Welcome Back
+          </h2>
+          <p className="w-11/12 text-gray-800 mb-6">
+            Welcome back to Smart Sight System. Log in to continue your
+            personalized experience and services.
+          </p>
+
+*/
--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
 import registerImg from "./../../assets/register-bg.jpg";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { useState } from "react";
 
 const Register = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div className="grid md:grid-cols-5 font-roboto">
       <div className="h-screen w-full flex items-center md:col-span-2 px-8 sm:px-10 md:px-0 md:ml-10">
@@ -9,18 +13,18 @@ const Register = () => {
           <h2 className="text-2xl sm:text-3xl md:text-4xl mb-2 font-bold">
             SIGN UP
           </h2>
-          <p className="w-11/12 text-gray-800 mb-6">
+          <p className="w-11/12 md:w-9/12 text-gray-800 mb-6 text-sm">
             welcome to the smart sight system for well deports register as a
             member to experience
           </p>
-          <form className="">
+          <form>
             <div>
               <label className="mb-1 text-sm">Name</label> <br />
               <input
                 type="text"
                 name="name"
                 placeholder="Name"
-                className="border px-4 py-2 rounded w-full md:w-2/3"
+                className="border px-4 py-2 rounded w-full md:w-2/3 placeholder:text-xs"
               />
             </div>
             <div className="my-2">
@@ -29,7 +33,7 @@ const Register = () => {
                 type="text"
                 name="photo"
                 placeholder="Photo URL"
-                className="border px-4 py-2 rounded w-full md:w-2/3"
+                className="border px-4 py-2 rounded w-full md:w-2/3 placeholder:text-xs"
               />
             </div>
             <div>
@@ -38,17 +42,23 @@ const Register = () => {
                 type="email"
                 name="email"
                 placeholder="Email"
-                className="border px-4 py-2 rounded w-full md:w-2/3"
+                className="border px-4 py-2 rounded w-full md:w-2/3 placeholder:text-xs"
               />
             </div>
-            <div className="mt-2">
+            <div className="mt-2 relative md:inline-block min-w-0 md:w-2/3">
               <label className="mb-1 text-sm">Password</label> <br />
               <input
-                type="password"
+                type={showPassword ? "text" : "password"}
                 name="password"
                 placeholder="Password"
-                className="border px-4 py-2 rounded w-full md:w-2/3"
+                className="border px-4 py-2 rounded w-full placeholder:text-xs"
               />
+              <span
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute inset-y-0 right-0 top-5 flex items-center pr-3 cursor-pointer"
+              >
+                {showPassword ? <AiFillEye /> : <AiFillEyeInvisible />}
+              </span>
             </div>
             <div className="mt-5 md:w-2/3">
               <input
