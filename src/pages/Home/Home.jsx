b378069b13de07c682bce89c0d1fcbd706631d0b
iffarurrahat <iffarurrahat@gmail.com>
2024-06-17 23:51:33 +0600

    Add CSS for AOS animation overflow-x

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,7 +8,13 @@ const Home = () => {
         className="my-[500px] text-2xl font-bold ml-10 data"
         data-aos="fade-left"
       >
-        Margin top: Lorem ipsum dolor sit amet.
+        Margin Left: Lorem ipsum dolor sit amet.
+      </p>
+      <p
+        className="my-[500px] text-2xl font-bold ml-10 data mt-32"
+        data-aos="fade-right"
+      >
+        Margin Right: Lorem ipsum dolor sit amet.
       </p>
     </div>
   );
